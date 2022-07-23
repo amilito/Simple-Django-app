1ea1401e2bf70e09413d3eb1ccf90c08437ce5e7
Adora <aneczkam0@gmail.com>
2022-07-23 15:20:36 +0200

    Amend settings

--- waste_segregation_project/settings.py
+++ waste_segregation_project/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
-# Heroku settings
-import django_heroku
-django_heroku.setting(locals())
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,3 +127,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Heroku settings
+import django_heroku
+django_heroku.setting(locals())
